942646cbeadbf1772e7429d30a64919661d66d34
Amirxon1601 <amirkxanturakxadjayev@gmail.com>
2023-03-28 14:53:18 +0500

    commit2

--- src/najottalim.java
+++ src/najottalim.java
@@ -2,5 +2,7 @@ public class najottalim {
     public static void main(String[] args) {
         System.out.println("Salom");
         System.out.println("Volekum");
+        System.out.println("Oka");
     }
 }
+
